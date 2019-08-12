f40e06ccbd92e63d61a819f87857d81278fd2e98
Igor Pereira <igor121093@hotmail.com>
2019-08-11 22:53:11 -0300

    SEMANA OMNISTACK

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,8 @@ const express = require('express');
 
 const mongoose = require('mongoose');
 
+
+
 const cors = require('cors');
 
 const routes = require('./routes'); // chama o arquivo de rotas
@@ -14,16 +16,20 @@ const routes = require('./routes'); // chama o arquivo de rotas
 // agora tenho acesso a biblioteca express, a express também é uma
 // função, toda vez que ela é chamada ela criar um servidor
 
-const server = express(); // chama a função express / servidor
+const app = express(); // chama a função express / servidor
+
+const server = require('http').Server(app);
+
+const io = require('socket.io');
 
 // inicia a coneção com o mongoose e passa a url
 // de conexão de importação do mongodb da aws
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-fcvm3.mongodb.net/omnistack8?retryWrites=true&w=majority', 
 {useNewUrlParser: true});
 
-server.use(cors());
-server.use(express.json()); // mostra para o express que vamos utilizar json no post
-server.use(routes); // add modulo/puglin
+app.use(cors());
+app.use(express.json()); // mostra para o express que vamos utilizar json no post
+app.use(routes); // add modulo/puglin
 
 // definir o localhost
 server.listen(3333);
